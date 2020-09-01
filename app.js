09d9b7668ce645d59deb7fc3ea7cb872a6e71af0
abdulloooh <abdullahakinwumi@gmail.com>
2020-09-01 11:31:40 +0100

    full error handling

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const winston = require("winston");
 const debug = require("debug")("mymoneymydebt-backend:server");
 const app = express();
 
-require("./startup/errorHandling")(debug, winston);
+require("./startup/errorHandler")(debug, winston);
 require("./startup/db")(debug);
 require("./startup/routes")(express, app, path);
 require("./startup/config")(app, path);
--- middlewares/asyncError.js
+++ middlewares/asyncError.js
@@ -2,5 +2,6 @@ const winston = require("winston");
 
 module.exports = function (err, req, res, next) {
   winston.error(err.message, err);
-  res.sendStatus(500);
+  // res.sendStatus(500);
+  res.status(err.status || 500).send(err.message || "Internal server error");
 };
