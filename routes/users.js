674c7e4e98cb8d1e2c5d74e0555a9cda9ddfeb4f
abdulloooh <abdullahakinwumi@gmail.com>
2020-09-09 14:28:17 +0100

    added user info update

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 const bcrypt = require("bcrypt");
 const _ = require("lodash");
+const auth = require("../middlewares/auth");
 const { validateUser, User } = require("../models/user");
 
 router.post("/", async (req, res, next) => {
@@ -25,4 +26,29 @@ router.post("/", async (req, res, next) => {
   res.header("x-auth-token", token).send(_.pick(user, ["username"]));
 });
 
+router.put("/", auth,  async (req, res, next) => {
+  const { error } = validateUser(req.body); //validate
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let user = await User.findOne({ username: req.body.username }); //check if username exists
+  if (user) return res.status(406).send("Username taken");
+
+  user = await User.findById(req.user._id) //check if userID exist
+  if(!user) return res.status(400).send("Invalid user")
+
+  if(req.body.username === req.body.password) return res.status(406).send("Use a more secure password");
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(req.body.password, salt); //hash password
+
+  user.username = req.body.username
+
+  await user.save();
+
+  const token = user.generateJwtToken();
+
+  res.header("x-auth-token", token).send(_.pick(user, ["username"]));
+});
+
 module.exports = router;
+
