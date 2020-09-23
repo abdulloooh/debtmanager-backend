6978806e41de41ae139ac73da597066fcc052299
abdulloooh <abdullahakinwumi@gmail.com>
2020-09-23 09:08:53 +0100

    failed to implement scripe for js

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const winston = require("winston");
 const debug = require("debug")("mymoneymydebt-backend:server");
 const app = express();
 
+require("@knuckleswtf/scribe-express")(app);
 require("./startup/prod")(app);
 require("./startup/errorHandler")(debug, winston);
 require("./startup/config")(app, path);
 require("./startup/db")(debug);
 require("./startup/routes")(express, app, path);
-require("./startup/doc")(app);
 
 module.exports = app;
--- startup/doc.js
+++
@@ -1,6 +0,0 @@
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("../swagger.json");
-
-module.exports = function (app) {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-};
