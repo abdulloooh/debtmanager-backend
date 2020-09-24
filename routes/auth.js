d6eaae86b077c0459ede3e01f8a7cae0790754f5
abdulloooh <abdullahakinwumi@gmail.com>
2020-09-24 11:12:19 +0100

    added change password

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const app = express();
 const bcrypt = require("bcrypt");
 const _ = require("lodash");
+const Joi = require("@hapi/joi");
 const { User, validateUser } = require("../models/user");
-
+const auth = require("../middlewares/auth");
 router.post("/", async (req, res, next) => {
   const { error } = validateUser(req.body); //validate
   if (error) return res.status(400).send(error.details[0].message);
@@ -29,6 +30,34 @@ router.post("/", async (req, res, next) => {
       secure: app.get("env") === "development" ? false : true,
     })
     .send(_.pick(user, ["username"]));
+});
+
+router.put("/password", auth, async (req, res) => {
+  const schema = Joi.object({
+    old_password: Joi.string().min(3).max(255).required(),
+    new_password: Joi.string().min(3).max(255).required(),
+  });
+
+  const { error } = schema.validate(req.body);
+
+  if (error) return res.status(406).send(error.details[0].message);
+
+  let user = await User.findById(req.user._id); //check if user exists
+  if (!user) return res.status(400).send("Invalid user");
+
+  //check if password matches if password exists
+  const validPassword = await bcrypt.compare(
+    req.body.old_password,
+    user.password
+  );
+  if (!validPassword) return res.status(406).send("Old Password incorrect");
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(req.body.new_password, salt); //hash password
+
+  await user.save();
+
+  res.clearCookie("access_token").sendStatus(200);
 });
 
 module.exports = router;
